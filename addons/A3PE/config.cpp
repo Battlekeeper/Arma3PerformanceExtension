78a7a9c15d381de55e75ea7980e690df562cd110
Sean <instantiateseankim@gmail.com>
2021-08-07 18:13:11 -0400

    Updated Styling in config.cpp

--- addons/A3PE/config.cpp
+++ addons/A3PE/config.cpp
@@ -1,7 +1,5 @@
-class CfgPatches
-{
-	class PiR
-	{
+class CfgPatches {
+	class PiR {
 		weapons[]={};
 		requiredVersion=2.02;
 		requiredAddons[]={};
@@ -10,35 +8,23 @@ class CfgPatches
 		units[]={};
 	};
 };
-class CfgFunctions
-{
-	class A3PE_functions_f
-	{
+class CfgFunctions {
+	class A3PE_functions_f {
 		tag="A3PE";
-		class Misc
-		{
+		class Misc {
 			file="\A3PE\Functions";
-			class toggle
-			{
-			};
-			class loop
-			{
-			};
-			class hide
-			{
-			};
+			class toggle {};
+			class loop {};
+			class hide {};
 		};
 	};
 };
-class Extended_PostInit_EventHandlers
-{
-	class A3PEInit_functions_f
-	{
+class Extended_PostInit_EventHandlers {
+	class A3PEInit_functions_f {
 		init="nul = [] execVM 'A3PE\functions\A3PEInit.sqf'";
 	};
 };
-class cfgMods
-{
+class cfgMods {
 	author="Battlekeeper";
 	timepacked="1628082446";
 };
